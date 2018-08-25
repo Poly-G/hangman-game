9f3e62724af46e4970c9601bf55e6b3706ecd2c4
Poly Goveia <poly.goveia@gmail.com>
2018-08-25 23:37:36 +0300

    add API GET request

--- README.md
+++ README.md
@@ -14,24 +14,6 @@ What things you need to install the software and how to install them
 Give examples
 ```
 
-### Installing
-
-A step by step series of examples that tell you how to get a development env running
-
-Say what the step will be
-
-```
-Give the example
-```
-
-And repeat
-
-```
-until finished
-```
-
-End with an example of getting some data out of the system or using it for a little demo
-
 
 ## Deployment
 
@@ -41,9 +23,9 @@ Add additional notes about how to deploy this on a live system
 
 Vanilla JavaScript, REST api 
 
-## Demo
+## Live Demo
 
-CLICK HERE for live demo 
+[DEMO](https://poly-g.github.io/hangman-game/)
 
 
 
--- js/app.js
+++ js/app.js
@@ -2,8 +2,11 @@ let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
     'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
     't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
+// Create a request variable and assign a new XMLHttpRequest object to it.
+let request = new XMLHttpRequest();
 
 
+// add the alphabet to the DOM
 function createButtons() {
     buttons = document.getElementById('buttons');
     ul = document.createElement('ul');
@@ -18,6 +21,37 @@ function createButtons() {
         ul.appendChild(list);
     }
 }
+createButtons();
 
-createButtons()
 
+
+
+
+
+// var xhr = new XMLHttpRequest();
+// xhr.withCredentials = true;
+
+// xhr.addEventListener("readystatechange", function () {
+//     if (this.readyState === 4) {
+//         console.log(this.responseText);
+//     }
+// });
+
+// xhr.open("GET", "http://app.linkedin-reach.io/words");
+// xhr.setRequestHeader("Cache-Control", "no-cache");
+
+// xhr.send();
+
+function handleSuccess() {
+    console.log(this.responseText);
+}
+
+function handleError() {
+    console.log('An error occurred');
+}
+
+const request = new XMLHttpRequest();
+request.open('GET', 'http://app.linkedin-reach.io/words');
+request.onload = handleSuccess;
+request.onerror = handleError;
+request.send();
