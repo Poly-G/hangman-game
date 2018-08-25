32e59f69cf244ae2ab8a9dda1b779348dfd05a76
Poly Goveia <poly.goveia@gmail.com>
2018-08-25 18:17:33 +0300

    add the alphabet to the dom

---
+++ js/app.js
@@ -0,0 +1,23 @@
+let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+    'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+let id = parent.id;
+
+function createButtons() {
+    buttons = document.getElementById('buttons');
+    ul = document.createElement('ul');
+
+    for (var i = 0; i < alphabet.length; i++) {
+        ul.id = 'alphabet';
+        list = document.createElement('li');
+        list.id = 'letter';
+        list.innerHTML = alphabet[i];
+
+        buttons.appendChild(ul);
+        ul.appendChild(list);
+    }
+}
+
+createButtons()
+
